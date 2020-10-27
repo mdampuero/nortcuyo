cd5785ef7c7e3b38d4a6410c9c1177e2ddb6448b
Mauricio Ampuero <mdampuero@gmail.com>
2020-10-26 21:38:34 -0300

    dump delete

--- docs/001_currency.sql
+++ docs/001_currency.sql
@@ -3,3 +3,7 @@ INSERT INTO `nortcuyo`.`currency` (`id`,`name`, `symbol`, `is_default`, `created
 
 #ALL Prouct
 update product set currency_id = '16474a68-0e6d-11eb-8905-3a54f5d23ebe';
+
+#ALL ORDERS
+INSERT INTO orders_total (SELECT uuid(),id,'16474a68-0e6d-11eb-8905-3a54f5d23ebe',total,total_vat,(total_vat - total),'2020-10-26 20:50:19','2020-10-26 20:50:19',0 FROM nortcuyo.orders);
+INSERT INTO orders_total (SELECT uuid(),id,'3061966a-0e6d-11eb-8905-3a54f5d23ebe',0,0,0,'2020-10-26 20:50:19','2020-10-26 20:50:19',0 FROM nortcuyo.orders);
